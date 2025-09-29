bb68386815c3b0ab53a6a6e43153228efdfa4ec6
Wacera <winniebanice@gmail.com>
2025-09-29 22:02:19 +0300

    update script

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ if(!passwordInput.length < 8){
 
 feedbackDiv.style.display = "block";
 if(isValid){
-    feedbackDiv.textContent = "Registration Successful!";
+    feedbackDiv.textContent = "Registration successful!";
     feedbackDiv.style.color = "#28a745";
 } else{
     feedbackDiv.innerHTML = messages.join("br>");
