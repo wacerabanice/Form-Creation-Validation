ffc2f437ec69f722bb13332d204cd42bbdd9aec5
Wacera <winniebanice@gmail.com>
2025-09-29 21:31:41 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a project on Advanced JavaScript!
+
+Learning 
+- Implement Form Validation.
+- Use DOM manipulation to dynamically interact with HTML elements.
+- Asynchronous JavaScript and APIs
+- Enhance User Experience
---
+++ script.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', function(){
+    console.log('DOM fully loaded and parsed');
+
+    const form = document.getElementById("registration-form");
+    const feedbackDiv = document.getElementById("form-feedback");
+
+    form.addEventListener('submit', function(event){
+        event.preventDefault();
+        console.log('Form Submitted');
+    });
+
+    const usernameInput = document.getElementById("username").value.trim();
+    const emailInput = document.getElementById("email").value.trim();
+    const passwordInput = document.getElementById("password").value.trim();
+
+    console.log('username: ', usernameInput);
+    console.log('email: ', emailInput);
+    console.log('password: ', passwordInput);
+
+let isValid = true;
+let messages = [];
+
+if(!username){
+    isValid = false;
+    messages.push('Username is required');
+}
+
+
+if(!emailInput.includes('@') | | !emailInput.includes('.')){
+    isValid = false;
+    messages.push('Provide a valid Email');
+}
+
+if(!passwordInput.length < 8){
+    isValid = false;
+    messages.push('Password must be atlease 8 characters long');
+}
+
+
+if(!isValid){
+    console.log('Validation errors: ', messages);
+} else{
+console.log('Form successfully submitted')
+}
+
+feedbackDiv.style.display = 'block';
+
+if(isValid){
+    feedbackDiv.textContent = "Registration Successful!";
+    feedbackDiv.style.color = rgba(64, 142, 100, 1);
+} else{
+    feedbackDiv.innerHTML = messages.join('<br>');
+    feedbackDiv.style.color = #dc3545;
+}
+
+
+});
