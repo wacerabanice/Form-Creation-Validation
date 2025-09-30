4a3c1348d560267659c95b63f317ec693af660c2
Wacera <winniebanice@gmail.com>
2025-09-30 19:57:06 +0300

    Add fetch files

---
+++ fetch-data.js
@@ -0,0 +1,26 @@
+async function fetchUserData() {
+    
+const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+const dataContainer = document.getElementById('api-data');
+
+try{
+    const response = await fetch(apiUrl);
+    const users = await response.json();
+
+   dataContainer.innerHTML = ''; 
+
+
+const userList = document.createElement('ul');
+users.forEach(user => {
+    const listItem = document.createElement('li');
+    userList.appendChild(listItem);
+});
+ dataContainer.appendChild(userList);
+}
+catch(error){
+    dataContainer.innerHTML='Failed to load user data.';
+    console.error('Error fetching user dat:', Error);
+}
+}
+
+document.addEventListener('DOMContentLoaded', fetchUserData);
